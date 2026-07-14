b1fa5c32fe5d87d813f191b03e433e1cf2c6f2f3
Anikesh Roy <anikeshroy62040@gmail.com>
2026-07-14 21:02:22 +0530

    First project initialized

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is my First React Project</p>
+      <p>I have started learning react from Sheriyans Coding School</p>
+      <ul>
+        <li>Anikesh</li>
+        <li>Rahul</li>
+      </ul>
+
+      <button>
+        <a href="https://www.anikeshroy.xyz">Go to my Github</a>
+      </button>
+
+      <div className="child-div">
+        <h2>This is test div</h2>
+      </div>
+    </div>
+  );
+}
+
+// export const NewFn = () => {
+//   return <p>This is my first react project</p>;
+// };
+
+export default App;
