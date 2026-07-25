882cc9ed54b071acbe6203d25cb3baf653d1caf5
Anikesh Roy <anikeshroy62040@gmail.com>
2026-07-25 14:28:17 +0530

    feat: add dummy navbar

---
+++ 18_context-api/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import NavbarItems from "./NavbarItems";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div>
+        <h1>Anikesh</h1>
+      </div>
+      <NavbarItems />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ 18_context-api/src/components/NavbarItems.jsx
@@ -0,0 +1,14 @@
+const NavbarItems = () => {
+  return (
+    <div className="navItems">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavbarItems;
